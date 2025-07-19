da90ae027dccad5983114ff5f61af162e9bfbac5
Centivex <abranhamscarnota@gmail.com>
2025-07-19 17:35:55 +0200

    Initial project structure set up

---
+++ src/main/java/com/example/backend_dev_test_app/BackendDevTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_dev_test_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDevTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDevTestAppApplication.class, args);
+	}
+
+}
